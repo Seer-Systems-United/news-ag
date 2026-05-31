913669a47ed8c641ed0d70ee8e23bbee9d1cdf8a
Nicholas-Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2026-05-31 17:56:40 -0400

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -5,3 +5,21 @@ pub struct Article {
     pub authors: Option<Vec<String>>,
     pub published_at: Option<chrono::DateTime<chrono::Utc>>,
 }
+
+impl Article {
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn url(&self) -> &str {
+        &self.url
+    }
+
+    pub fn authors(&self) -> Option<&[String]> {
+        self.authors.as_deref()
+    }
+
+    pub fn published_at(&self) -> Option<chrono::DateTime<chrono::Utc>> {
+        self.published_at
+    }
+}
